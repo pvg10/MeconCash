6c1288c4d1513660520202c0b37252f63fe6af5b
Aarish Hussain <52711144+aarish-git@users.noreply.github.com>
2021-04-02 22:45:34 +0530

    updated faq layout

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -366,33 +366,19 @@ function Login() {
               {/* <Faq data={faqs} config = {{
                 animate :true,}
               }/> */}
-              <div id="accordion">
-                <div className="card">
-                  <div className="card-header"  data-toggle="collapse"
-                      data-target="#collapseOne" id="headingOne">
-                    <h5
-                      className="mb-0"
-                     
-                      aria-expanded="true"
-                    >
-                      <button
-                        className="btn btn-link"
-                        aria-controls="collapseOne"
-                      >
-                        What is a decentralized exchange, and what makes it
-                        different?
-                      </button>
-                    </h5>
-                  </div>
+<div id="main">
+  <div class="container">
+<div class="accordion" id="faq">
+                    <div class="card">
+                        <div class="card-header" id="faqhead1">
+                            <a href="#" class="btn btn-header-link" data-toggle="collapse" data-target="#faq1"
+                            aria-expanded="true" aria-controls="faq1"> What is a decentralized exchange, and what makes it
+                        different?</a>
+                        </div>
 
-                  <div
-                    id="collapseOne"
-                    className="collapse show container"
-                    aria-labelledby="headingOne"
-                    data-parent="#accordion"
-                  >
-                    <div className="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
+                        <div id="faq1" class="collapse show" aria-labelledby="faqhead1" data-parent="#faq">
+                            <div class="card-body">
+                               Anim pariatur cliche reprehenderit, enim eiusmod high life
                       accusamus terry richardson ad squid. 3 wolf moon officia
                       aute, non cupidatat skateboard dolor brunch. Food truck
                       quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
@@ -403,118 +389,63 @@ function Login() {
                       lomo. Leggings occaecat craft beer farm-to-table, raw
                       denim aesthetic synth nesciunt you probably haven't heard
                       of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="card-header"  data-toggle="collapse"
-                      data-target="#collapseTwo" id="headingTwo">
-                    <h5
-                      className="mb-0 "
-                     
-                      aria-expanded="false"
-                    >
-                      <button
-                        className="btn btn-link collapsed"
-                        aria-controls="collapseTwo"
-                      >
-                        How does your trading price and speed compare to others?
-                      </button>
-                    </h5>
-                  </div>
-                  <div
-                    id="collapseTwo"
-                    className="collapse container"
-                    aria-labelledby="headingTwo"
-                    data-parent="#accordion"
-                  >
-                    <div className="card-body">
-                      Using Layer 2 for transactions allows us for seamless
-                      transfers for our customers with close to zero gas fees
-                      included. With the average time of transaction being
-                      around 30 seconds and the cost of over $6 for Ethereum on
-                      various exchanges available on the market, we were able to
-                      make it literally free with our gas station network and
-                      approximately 1~2 seconds for an average transaction.
-                    </div>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="card-header"   data-toggle="collapse"
-                      data-target="#collapseThree" id="headingThree">
-                    <h5
-                      className="mb-0"
-                     
-                      aria-expanded="false"
-                    >
-                      <button
-                        className="btn btn-link collapsed"
-                        aria-controls="collapseThree"
-                      >
-                        How can I make money with MCFinance?
-                      </button>
-                    </h5>
-                  </div>
-                  <div
-                    id="collapseThree"
-                    className="collapse"
-                    aria-labelledby="headingThree"
-                    data-parent="#accordion"
-                  >
-                    <div className="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
-                      accusamus terry richardson ad squid. 3 wolf moon officia
-                      aute, non cupidatat skateboard dolor brunch. Food truck
-                      quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                      tempor, sunt aliqua put a bird on it squid single-origin
-                      coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                      helvetica, craft beer labore wes anderson cred nesciunt
-                      sapiente ea proident. Ad vegan excepteur butcher vice
-                      lomo. Leggings occaecat craft beer farm-to-table, raw
-                      denim aesthetic synth nesciunt you probably haven't heard
-                      of them accusamus labore sustainable VHS.
-                    </div>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="card-header"   data-toggle="collapse"
-                      data-target="#collapseFour" id="headingFour">
-                    <h5
-                      className="mb-0 "
-                     
-                      aria-expanded="false"
-                    >
-                      <button
-                        className="btn btn-link collapsed"
+                    <div class="card">
+                        <div class="card-header" id="faqhead2">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq2"
+                            aria-expanded="true" aria-controls="faq2">How does your trading price and speed compared to others?</a>
+                        </div>
 
-                        // aria-controls="collapseThree"
-                      >
-                        What is the MCF token?
-                      </button>
-                    </h5>
-                  </div>
-                  <div
-                    id="collapseFour"
-                    className="collapse container"
-                    aria-labelledby="headingThree"
-                    data-parent="#accordion"
-                  >
-                    <div className="card-body">
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
-                      accusamus terry richardson ad squid. 3 wolf moon officia
-                      aute, non cupidatat skateboard dolor brunch. Food truck
-                      quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                      tempor, sunt aliqua put a bird on it squid single-origin
-                      coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                      helvetica, craft beer labore wes anderson cred nesciunt
-                      sapiente ea proident. Ad vegan excepteur butcher vice
-                      lomo. Leggings occaecat craft beer farm-to-table, raw
-                      denim aesthetic synth nesciunt you probably haven't heard
-                      of them accusamus labore sustainable VHS.
+                        <div id="faq2" class="collapse" aria-labelledby="faqhead2" data-parent="#faq">
+                            <div class="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                    <div class="card">
+                        <div class="card-header" id="faqhead3">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq3"
+                            aria-expanded="true" aria-controls="faq3">  How can I make money with MCFinance?</a>
+                        </div>
+
+                        <div id="faq3" class="collapse" aria-labelledby="faqhead3" data-parent="#faq">
+                            <div class="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+  <div class="card">
+                        <div class="card-header" id="faqhead3">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq4"
+                            aria-expanded="true" aria-controls="faq3"> What is the MCF token?</a>
+                        </div>
+
+                        <div id="faq4" class="collapse" aria-labelledby="faqhead3" data-parent="#faq">
+                            <div class="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+                                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+    </div>
+  </div>
             </div>
           </div>
         </section>
